f67ae3713e6a000d67311d935e13d91345049d34
Priyanshu Kumar <meetpgat8434@gmail.com>
2026-07-20 14:21:18 +0530

    pay changes

--- gitandgithub/second.js
+++ gitandgithub/second.js
@@ -1,6 +1,6 @@
 console.log(" git graph extension is imp ")
 const name="rohit"
-const age=40
+const age=90
 const balance=900
 
 const account =57487494
@@ -8,6 +8,9 @@ const account =57487494
 a=32
 b=98
 console.log(a+b);
+function updatebalance(){
+    balance+=20;
+}
 
 console.log("bug final commit")
 console.log("bug file creation ")
@@ -16,3 +19,4 @@ function showage(){
     console.log(age)
 }
 showage();
+
