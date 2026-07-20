af8d27d904c89ef91adf986b87d8a36122c10867
Priyanshu Kumar <meetpgat8434@gmail.com>
2026-07-20 14:13:51 +0530

    new changes

--- gitandgithub/second.js
+++ gitandgithub/second.js
@@ -8,10 +8,11 @@ const account =57487494
 a=32
 b=98
 console.log(a+b);
-<<<<<<< HEAD
+
 console.log("bug final commit")
-=======
-
-
 console.log("bug file creation ")
->>>>>>> bug
+
+function showage(){
+    console.log(age)
+}
+showage();
